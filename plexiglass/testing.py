635782c1e251fa5aa89e1b9dc7e40ba1e21ffa2e
kanxx030 <kanxx030@gmail.com>
2020-11-12 11:34:05 -0600

    added normal run in test_robustness

--- plexiglass/testing.py
+++ plexiglass/testing.py
@@ -2,6 +2,19 @@ import torch
 from adversarial.fgsm import FGSM
 
 def test_robustness(model, attack, dataloader, device, eps=None):
+
+    # normal run 
+    for images, labels in dataloader:
+            outputs = model(images.to(device))
+            labels = labels.to(device)
+
+            _, pre = torch.max(outputs.data, 1)
+            total += 1
+            correct += (pre == labels).sum()
+    print('Accuracy: '+ str(100 * float(correct) / total))
+
+    # attack run 
+
     if isinstance(model, FGSM()):
         for ep in eps:
             for images, labels in dataloader:   
@@ -12,14 +25,14 @@ def test_robustness(model, attack, dataloader, device, eps=None):
                 _, pre = torch.max(outputs.data, 1)
                 total += 1
                 correct += (pre == labels).sum()
-                print('Epsilon: '+str(ep)+' Accuracy: '+str(100 * float(correct) / total))
+            print('Epsilon: '+str(ep)+' Accuracy: '+str(100 * float(correct) / total))
+    else:
+        for images, labels in dataloader:
+            perturbed_images = attack(images, labels).to(device)
+            outputs = model(perturbed_images)
+            labels = labels.to(device)
 
-    for images, labels in dataloader:
-        perturbed_images = attack(images, labels).to(device)
-        outputs = model(perturbed_images)
-        labels = labels.to(device)
-
-        _, pre = torch.max(outputs.data, 1)
-        total += 1
-        correct += (pre == labels).sum()
-    print('Accuracy: '+ str(100 * float(correct) / total))
+            _, pre = torch.max(outputs.data, 1)
+            total += 1
+            correct += (pre == labels).sum()
+        print('Accuracy: '+ str(100 * float(correct) / total))
