671314bd3a5e7eb3f0186ad54cf7a8300f2f8bec
kanxx030 <kanxx030@gmail.com>
2020-11-12 01:50:11 -0600

    updated versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="plexiglass",
-    version="1.0.0",
+    version="1.0.1",
     description="PyTorch toolbox for cybersecurity research and testing against adversarial attacks and deepfakes.",
     url="https://github.com/enochkan/safetynet",
     author="Chi Nok Enoch Kan @chinokenochkan",
