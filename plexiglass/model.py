a62d02ea9c640a77e9c352331a234ff113278cdc
zhaohan_dong <65422392+zhaohan-dong@users.noreply.github.com>
2023-12-08 10:32:22 +0000

    Modified hf to use pipeline

--- plexiglass/model.py
+++ plexiglass/model.py
@@ -1,5 +1,5 @@
 from litellm import completion, ModelResponse, Usage, Choices, Message
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline
 
 supported_list = ["openai", "hf"]
 
@@ -11,31 +11,16 @@ class Model:
             self.model_name = model_name
         if self.model_type == "hf":
             # huggingface model
-            self._tokenizer = AutoTokenizer.from_pretrained(model_name)
-            self._model = AutoModelForCausalLM.from_pretrained(model_name)
+            self._hf_pipeline = pipeline("text-generation", model=model_name)
         else:
             raise ValueError("Unsupported model type")
 
-    def prompt(self, messages: list) -> ModelResponse:
+    def prompt(self, messages: list):
+        # TODO: check multiple messages input
         if self.model_type != "hf":
-            response = completion(self.model_name, messages=messages)
+            response = completion(self.model_name, messages=messages)["choices"][0]["message"]["content"]
         elif self.model_type == "hf":
-
-            # Text generation
-            input_ids = self._tokenizer.encode(messages[0], return_tensors="pt")
-            output = self._model.generate(input_ids)
-            generated_text = self._tokenizer.decode(output[0])
-
-            # Usage
-            prompt_tokens = len(self._tokenizer.tokenize(messages[0]))
-            total_tokens = len(self._tokenizer.tokenize(generated_text))
-
-            response = ModelResponse(model=self.model_name,
-                                     usage=Usage(completion_tokens=total_tokens - prompt_tokens,
-                                                 prompt_tokens=prompt_tokens,
-                                                 total_tokens=total_tokens))
-            response.choices = Choices(message=Message(content=generated_text))
-
+            response = self._hf_pipeline(messages)[0][0]["generated_text"]
         else:
             raise ValueError("Unsupported model type")
         return response
