096904781e2fcd8100e76c5fcdd579b8d8120e2c
Zhaohan Dong <65422392+zhaohan-dong@users.noreply.github.com>
2023-12-08 23:58:26 +0000

    Zhaohan/hf (#13)
    * Added hf
    
    * Modified hf to use pipeline
    
    * Added transformers dependency
    
    * Updated dependency and changed output typing

--- plexiglass/model.py
+++ plexiglass/model.py
@@ -1,22 +1,25 @@
 from litellm import completion
+from transformers import pipeline
 
-supported_list = ["openai"]
+supported_list = ["openai", "hf"]
+
 
 class Model:
     def __init__(self, model_type: str, model_name: str):
         if model_type in supported_list:
             self.model_type = model_type
             self.model_name = model_name
-        elif model_type == "hf":
-             # huggingface model
-             pass
+            if self.model_type == "hf":
+                # huggingface model
+                self._hf_pipeline = pipeline("text-generation", model=model_name, device_map="auto")
         else:
             raise ValueError("Unsupported model type")
 
-    def prompt(self, messages: list):
-            if self.model_type != "hf":
-                response = completion(self.model_name, messages=messages)
-                return response['choices'][0]["message"]["content"]
-            else:
-               # run_hf_inference()
-               pass
+    def prompt(self, messages: list) -> str:
+        if self.model_type != "hf":
+            response = completion(self.model_name, messages=messages)["choices"][0]["message"]["content"]
+        elif self.model_type == "hf":
+            user_msg = "\n\n".join([m["content"] for m in messages])
+            response = self._hf_pipeline(user_msg)[0]["generated_text"]
+
+        return response
